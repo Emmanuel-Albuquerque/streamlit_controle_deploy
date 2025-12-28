df0604bf713ab21b20d0f497476de70d8ab7cabd
Emmanuel-Albuquerque <emanoag@gmail.com>
2025-12-28 13:05:54 -0300

    Update definitivo.py

--- definitivo.py
+++ definitivo.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Sheet1", ttl=0)
 
-st.write(df)
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
