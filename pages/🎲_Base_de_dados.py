bfffc7abbe3192c6a3650cf0a75abe27ce04f0ff
Emmanuel-Albuquerque <emanoag@gmail.com>
2025-12-28 14:19:49 -0300

    Update 🎲_Base_de_dados.py

--- pages/🎲_Base_de_dados.py
+++ pages/🎲_Base_de_dados.py
@@ -1,2 +1,9 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
+# conexão planilha
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(worksheet="Página1", ttl=0)
+
+st.dataframe(df)
